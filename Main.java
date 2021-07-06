b2b8cfcb23dd6d7a06a0a32c1b2ed0eb957fb8de
aleksyniemir <73692634+aleksyniemir@users.noreply.github.com>
2021-07-06 23:54:25 +0200

    javafx okno

--- Main.java
+++ Main.java
@@ -4,22 +4,31 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.Stack;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        StackPane stackPane = new StackPane();
+        Button button = new Button("Przycisk");
+        stackPane.getChildren().add(button);
+
+        Scene scene = new Scene(stackPane, 400, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Snake");
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
 
-        //launch(args); przydatne w fazie robienia okienka
+        launch(args);
         System.out.println("aa");
     }
 
