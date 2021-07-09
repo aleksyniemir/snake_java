7696648724c76bdb03dca6f541c86476dbf0a355
aleksyniemir <73692634+aleksyniemir@users.noreply.github.com>
2021-07-09 14:51:03 +0200

    pierwsze okienko, sample.fxml i controller

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.util.Stack;
 
@@ -14,12 +16,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        StackPane stackPane = new StackPane();
-        Button button = new Button("Przycisk");
-        stackPane.getChildren().add(button);
 
-        Scene scene = new Scene(stackPane, 400, 600);
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("/sample/sample.fxml"));
+        StackPane stackPane = loader.load();
 
+        Scene scene = new Scene (stackPane);
+
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Snake");
         primaryStage.show();
@@ -29,7 +33,7 @@ public class Main extends Application {
     public static void main(String[] args) {
 
         launch(args);
-        System.out.println("aa");
+
     }
 
 
